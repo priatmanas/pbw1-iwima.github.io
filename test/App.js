a012f4913c83c94327c80dd2f1596a033f5ed8f6
Fauzan Priatmana <fauzanpriatmanas@gmail.com>
2025-07-03 10:40:06 +0700

    Upload file test

---
+++ test/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="grid-container">
+      <div class="item1">
+        <h1>Pemrograman berbasis Web 1</h1>
+      </div>
+      <div class="item2">
+        <ul>
+          <li>
+            <a href="../pertemuan8">Home</a>
+          </li>
+          <li>
+            <a href="materi.html">Materi</a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="item3">
+        <h1>Pokok Materi</h1>
+        <ol>
+          <li>Web Development</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+        </ol>
+      </div>
+      <div class="item5">
+        <p>Tahun Ajaran Genap 2024-2025</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
